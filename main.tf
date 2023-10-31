c636e09e152c2be3d509285ab1b7c2e3cc72afd5
Gangadharrao08 <143478731+Gangadharrao08@users.noreply.github.com>
2023-10-31 22:45:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ region = "us-east-1"
 resource "aws_instance" "one" {
   ami             = "ami-01eccbf80522b562b"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "Terraformnew"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1a"
   user_data       = <<EOF
@@ -25,7 +25,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-01eccbf80522b562b"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "Terraformnew"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1b"
   user_data       = <<EOF
@@ -44,7 +44,7 @@ EOF
 resource "aws_instance" "three" {
   ami             = "ami-01eccbf80522b562b"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "Terraformnew"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1a"
   tags = {
@@ -55,7 +55,7 @@ resource "aws_instance" "three" {
 resource "aws_instance" "four" {
   ami             = "ami-01eccbf80522b562b"
   instance_type   = "t2.micro"
-  key_name        = "rahamabc"
+  key_name        = "Terraformnew"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1"
   tags = {
