45a20fd5129bcf3dd9704f3bc968ce92ccd54a4b
Gangadharrao08 <143478731+Gangadharrao08@users.noreply.github.com>
2023-11-01 07:08:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "devopsbygangaterraserverbucket8326"
+  bucket = "devopsbygangaterraserverbucket832678"
 }
 
 resource "aws_iam_user" "seven" {
